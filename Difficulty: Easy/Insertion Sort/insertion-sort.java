e685f98c33ca5c710d6f277ad24cd4d7a04b060f
Rajeswari Katasani <158117922+Rajeswari712@users.noreply.github.com>
2025-01-27 16:46:54 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.java
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -44,17 +44,45 @@ class Solution {
     public void insertionSort(int arr[]) {
         // code here
         int n = arr.length;
-        for(int i=2;i<=n;i++)
+        // for(int i=2;i<=n;i++)
+        // {
+        //     for(int j=i-1;j>0;j--)
+        //     {
+        //         if(arr[j]<arr[j-1])
+        //         {
+        //             int temp=arr[j];
+        //             arr[j]=arr[j-1];
+        //             arr[j-1]=temp;
+        //             System.out.println("swapped ");
+        //         }
+        //     }
+        // }
+        
+        for(int i=1;i<=n-1;i++)
         {
-            for(int j=i-1;j>0;j--)
+            for(int j=i;j>0;j--)
             {
                 if(arr[j]<arr[j-1])
                 {
-                    int temp=arr[j];
+                    int temp = arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    // System.out.println("swapped ");
                 }
             }
         }
+        
+        // for(int i=0;i<=n-1;i++)
+        // {
+        //     int j = i;
+        //     while(j>0 && arr[j-1] > arr[j])
+        //     {
+        //         int temp=arr[j-1];
+        //         arr[j-1]=arr[j];
+        //          arr[j]=temp;
+        //          j--;
+        //           System.out.println("swapped ");
+        //     }
+        // }
     }
 }
